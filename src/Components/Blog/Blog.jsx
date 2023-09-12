17cbf79e051d2f1065022e108505f5c22555932c
shahajalal-shawon <shahajalalsheikh001@gmail.com>
2023-09-13 03:48:50 +0600

    added the blog components on jsx file

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({ blog }) => {
+    console.log(blog);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+// Blog.proptypes = {
+//     blog: PropTypes.object.isRequired
+// }
+
+Blog.propTypes ={
+    blog: PropTypes.object.isRequired
+}
+
+export default Blog;
